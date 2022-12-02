f5c61b4b729a317b569027e04e8d1d07208418aa
czheo <czheo1987@gmail.com>
2022-12-01 23:01:01 -0800

    day 2 part 2

--- day_02/main.rs
+++ day_02/main.rs
@@ -1,53 +1,55 @@
 use std::io;
-use std::cmp::{Ordering, Ord, PartialOrd};
+use std::cmp::{Ordering};
 
-#[derive(PartialEq, Eq)]
+
+#[derive(Copy, Clone)]
 enum Action {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
 }
 
-impl PartialOrd for Action {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Action {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match (self, other) {
-            (Action::Rock, Action::Paper) => Ordering::Less,
-            (Action::Paper, Action::Scissors) => Ordering::Less,
-            (Action::Scissors, Action::Rock) => Ordering::Less,
-            _ if self == other => Ordering::Equal,
-            _ => Ordering::Greater,
-        }
-    }
-}
+const ACTION_ARRAY: [Action; 3] = [Action::Rock, Action::Paper, Action::Scissors];
 
 fn main() {
     let mut score = 0;
     for l in io::stdin().lines() {
         let line = l.unwrap();
         let mut it = line.split_ascii_whitespace().take(2);
-        let x = parse(it.next().unwrap());
-        let y = parse(it.next().unwrap());
-        if x < y {
-            score += 6;
-        } else if x == y {
+        let x = parse_action(it.next().unwrap());
+        let y = parse_result(it.next().unwrap());
+        if y == Ordering::Equal{
             score += 3;
+        } else if y == Ordering::Greater {
+            score += 6;
         }
-        score += y as u32;
+        score += get_action(x, y) as u32;
     }
     println!("{}", score);
 }
 
-fn parse(s : &str) -> Action {
+fn get_action(action: Action, result: Ordering) -> Action {
+    match result {
+        Ordering::Equal => action,
+        Ordering::Less => ACTION_ARRAY[(action as i32 - 2).rem_euclid(3) as usize],
+        Ordering::Greater => ACTION_ARRAY[(action as i32).rem_euclid(3) as usize],
+    }
+}
+
+fn parse_action(s: &str) -> Action {
     match s {
-        "A" | "X" => Action::Rock,
-        "B" | "Y" => Action::Paper,
-        "C" | "Z" => Action::Scissors,
+        "A" => Action::Rock,
+        "B" => Action::Paper,
+        "C" => Action::Scissors,
+        _ => panic!("Unknown input: {}", s)
+    }
+}
+
+fn parse_result(s: &str) -> Ordering {
+    match s {
+        "X" => Ordering::Less,
+        "Y" => Ordering::Equal,
+        "Z" => Ordering::Greater,
         _ => panic!("Unknown input: {}", s)
     }
 }
