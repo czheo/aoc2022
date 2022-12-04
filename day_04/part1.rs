c893a05e7b3a88499be93067bcdf11b00be487bc
czheo <czheo1987@gmail.com>
2022-12-03 21:30:19 -0800

    day 4

---
+++ day_04/part1.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+
+fn main() {
+    let mut ans = 0;
+    for l in io::stdin().lines() {
+        let line = l.unwrap();
+        let mut it = line.split(',').take(2);
+        let (f1, t1) = parse(it.next().unwrap());
+        let (f2, t2) = parse(it.next().unwrap());
+        if (f1 <= f2 && t1 >= t2) || (f2 <= f1 && t2 >= t1) {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn parse(s: &str) -> (u8, u8) {
+    let mut it = s.split('-').take(2);
+    let p1 = it.next().unwrap().parse().unwrap();
+    let p2 = it.next().unwrap().parse().unwrap();
+    (p1, p2)
+}
