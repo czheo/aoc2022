e6a3b09cac96550cd8089fceca4af0dc667f88bc
czheo <1692400+czheo@users.noreply.github.com>
2022-12-08 07:01:19 -0800

    day 8 part 2

--- day_08/main.rs
+++ day_08/main.rs
@@ -1,58 +1,58 @@
 fn main() {
-    let (input, mut visibility) = init();
+    let input = init();
     let (m, n) = (input.len(), input[0].len());
-    for i in 0..m {
-        let mut max = -1;
-        for j in (0..n).rev() {
-            if input[i][j] > max {
-                visibility[i][j] = true;
-            }
-            max = std::cmp::max(max, input[i][j]);
+    let mut max = 0;
+    for i in 1..m-1 {
+        for j in 1..n-1 {
+            max = std::cmp::max(max, score(&input, i, j, m, n));
         }
     }
-    for j in 0..n {
-        let mut max = -1;
-        for i in 0..m {
-            if input[i][j] > max {
-                visibility[i][j] = true;
-            }
-            max = std::cmp::max(max, input[i][j]);
-        }
-        max = -1;
-        for i in (0..m).rev() {
-            if input[i][j] > max {
-                visibility[i][j] = true;
-            }
-            max = std::cmp::max(max, input[i][j]);
-        }
-    }
-    let mut ans = 0;
-    for i in 0..m {
-        for j in 0..n {
-            if visibility[i][j] == true {
-                ans += 1;
-            }
-        }
-    }
-    println!("{}", ans);
+    println!("{}", max);
 }
 
-fn init() -> (Vec<Vec<i8>>, Vec<Vec<bool>>) {
-    let mut input: Vec<Vec<i8>> = Vec::new();
-    let mut visibility: Vec<Vec<bool>> = Vec::new();
-    for l in std::io::stdin().lines() {
-        let mut ri = Vec::new();
-        let mut rv = Vec::new();
-        let mut max: i8 = -1;
-        for c in l.unwrap().chars() {
-            let n = c.to_digit(10).unwrap() as i8;
-            ri.push(n);
-            rv.push(n > max);
-            max = std::cmp::max(max, n);
+fn score(input: &Vec<Vec<u32>>, i: usize, j: usize, m: usize, n: usize) -> usize{
+    let mut score = 1;
+    let mut x = i + 1;
+    while x < m - 1 {
+        if input[x][j] >= input[i][j] {
+            break;
         }
-        input.push(ri);
-        visibility.push(rv);
+        x += 1;
     }
-    (input, visibility)
+    score *= x - i;
+    x = i - 1;
+    while x > 0 {
+        if input[x][j] >= input[i][j] {
+            break;
+        }
+        x -= 1;
+    }
+    score *= i - x;
+    x = j + 1;
+    while x < n - 1{
+        if input[i][x] >= input[i][j] {
+            break;
+        }
+        x += 1;
+    }
+    score *= x - j;
+    x = j - 1;
+    while x > 0 {
+        if input[i][x] >= input[i][j] {
+            break;
+        }
+        x -= 1;
+    }
+    score *= j - x;
+    score
+}
+
+fn init() -> Vec<Vec<u32>> {
+    std::io::stdin().lines()
+        .map(|l|
+            l.unwrap().chars()
+            .map(|c| c.to_digit(10).unwrap())
+            .collect())
+        .collect()
 }
 
